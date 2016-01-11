2650ae4f7c4d3b3e035657e6e3cee062ea5ba52a
Mark Old <dlom234@gmail.com>
2016-01-10 21:29:14 -0800

    add capability of seeding initial intersections

--- intersections.js
+++ intersections.js
@@ -1,45 +1,46 @@
 var intersect = function(a, b) {
+    if (a.length === 0) return b;
     return a.filter(function(x) {
         return b.indexOf(x) >= 0;
     });
 };
 
-module.exports = function(groupSize, intersectionSize, getTable, callback) {
+module.exports = function(initialGroup, groupSize, intersectionsSize, getTable, callback) {
+    if (initialGroup.length < 1) return callback(new Error("initialGroup must have at least 1 set of data"));
     if (groupSize < 2) return callback(new Error("groupSize must be at least 2"));
-    if (intersectionSize < 1) return callback(new Error("intersectionSize must be at least 1"));
+    if (intersectionsSize < 1) return callback(new Error("intersectionsSize must be at least 1"));
 
     var group = [];
     var intersections = [];
 
+    initialGroup.forEach(function(initial) {
+        group.push(initial.id);
+        intersections = intersect(intersections, initial.data);
+    });
+
+    if (intersections.length < intersectionsSize) return callback(new Error("initialGroup does not have enough intersections"));
+
+    if (group.length >= groupSize) {
+        // TODO shuffle intersections first
+        return callback(null, group, intersections.slice(0, intersectionsSize));
+    }
+
     function asyncLoop() {
         getTable(function(err, table) {
             if (err) return callback(err);
+            if (table.length == 0) return callback("no valid intersections found", group, intersections);
             var i = 0;
 
-            // find initial seed
-            while (group.length === 0) {
-                var initial = table[i++];
-                // if initial has enough things
-                if (initial.data.length >= intersectionSize) {
-                    intersections = initial.data;
-                    group.push(initial.id);
-                } else {
-                    if (i >= table.length) {
-                        return setImmediate(asyncLoop);
-                    }
-                }
-            }
-
-            // loop through available table
             while (i < table.length) {
                 var next = table[i++];
+
                 if (group.indexOf(next.id) < 0) {
                     var nextIntersections = intersect(intersections, next.data);
-                    // if we have a good intersection
-                    if (nextIntersections.length >= intersectionSize) {
+
+                    if (nextIntersections.length >= intersectionsSize) {
                         intersections = nextIntersections;
                         group.push(next.id);
-                        // if we have enough group
+
                         if (group.length >= groupSize) {
                             return callback(null, group, intersections);
                         }
@@ -51,5 +52,5 @@ module.exports = function(groupSize, intersectionSize, getTable, callback) {
         });
     }
 
-    setImmediate(asyncLoop);
+    asyncLoop();
 };

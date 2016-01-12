496a6763a356aa338d8d6a3b9a27851ffe036097
Mark Old <dlom234@gmail.com>
2016-01-12 01:25:07 -0800

    shitty tests wtv

--- test/intersections.js
+++ test/intersections.js
@@ -7,14 +7,19 @@ var HYPOTHETICAL_USERS_IN_DB = 30000;
 var GROUP_SIZE = 8;
 var SEASON_SIZE = 7;
 
-var createGroup = require("../index");
+var tape = require("tape");
+var findIntersections = require("../index");
 var fakedb = require("./fakedb")(NUMBER_OF_SHOWS, AVERAGE_PTW_SIZE, HYPOTHETICAL_USERS_IN_DB);
 
-createGroup([fakedb.createSeed()], GROUP_SIZE, SEASON_SIZE, fakedb(BATCH_SIZE), function(err, group, intersections, isIncomplete) {
-    if (err) throw err;
-    console.log(group);
-    console.log(intersections);
-    if (isIncomplete) {
-        console.log("not enough valid intersections found");
-    }
+tape("findIntersections", function(t) {
+    t.plan(1);
+    findIntersections([fakedb.createSeed()], GROUP_SIZE, SEASON_SIZE, fakedb(BATCH_SIZE), function(err, group, intersections, isIncomplete) {
+        if (err) throw err;
+        console.log(group);
+        console.log(intersections);
+        if (isIncomplete) {
+            console.log("not enough valid intersections found");
+        }
+        t.assert(intersections.length <= SEASON_SIZE);
+    });
 });
